d9c53c64c21ad4a97a40b57927c4401097582e14
Tomas Vosylius <vsls.tomas@gmail.com>
2020-10-22 11:19:17 +0300

    Speed mechanics

--- src/App.js
+++ src/App.js
@@ -5,32 +5,64 @@ import Throttle from "./components/Throttle";
 
 const minRPM = 2000;
 const maxRPM = 8000;
+const maxSpeed = 120;
 
 function App() {
   const [throttle, setThrottle] = useState(false);
   const [RPM, setRPM] = useState(minRPM);
+  const [gear, setGear] = useState(1);
+  const [speed, setSpeed] = useState(0);
 
   useEffect(() => {
+    const increaseSpeed = () => {
+      let newSpeed = speed + maxSpeed / Math.max(1, speed) / 30;
+      setSpeed(Math.min(maxSpeed, newSpeed));
+    };
+
+    const decreaseSpeed = () => {
+      setSpeed(Math.max(0, speed - 0.35));
+    };
+
+    const addGear = (value) => {
+      if (value < 0) {
+        setRPM(maxRPM);
+      } else if (value !== gear) {
+        setRPM(minRPM);
+      }
+      setGear(gear + value);
+    };
+
     const interval = setInterval(() => {
       if (!throttle) {
-        if (RPM <= minRPM) return;
+        if (RPM > minRPM) {
+          setRPM(Math.max(minRPM, RPM - 10));
+        }
 
-        setRPM(Math.max(minRPM, RPM - 10));
+        if (speed > 0) {
+          decreaseSpeed();
+          if (RPM <= minRPM) addGear(-1);
+        }
         return;
       }
 
-      if (RPM >= maxRPM) return;
+      if (RPM < maxRPM) {
+        setRPM(Math.min(maxRPM, RPM + 25));
+      } else {
+        addGear(1);
+      }
 
-      setRPM(Math.min(maxRPM, RPM + 25));
+      if (speed < maxSpeed) {
+        increaseSpeed();
+      }
     }, 10);
 
     return () => clearInterval(interval);
-  }, [RPM, throttle]);
+  }, [RPM, throttle, speed, gear]);
 
   return (
     <div className="App">
       <Throttle throttle={throttle} setThrottle={setThrottle} />
-      <Body RPM={RPM} />
+      <Body RPM={RPM} gear={gear} speed={speed} />
     </div>
   );
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./Body.css";
 
 export default function Body(props) {
-  return <div className="Body">{props.RPM}</div>;
+  return (
+    <div className="Body">
+      <h3>{props.gear}</h3>
+      <h4>{Math.round(props.speed)} mp/h</h4>
+      <small>{props.RPM} RPM</small>
+    </div>
+  );
 }
